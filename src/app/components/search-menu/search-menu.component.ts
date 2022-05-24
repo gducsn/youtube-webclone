a6268c6d81ecefca67d542e22bc9be41cec08916
gducsn <gducsn@gmail.com>
2022-05-24 14:21:57 +0200

    nav responsive

--- src/app/components/search-menu/search-menu.component.ts
+++ src/app/components/search-menu/search-menu.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit} from '@angular/core';
-import { of } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, fromEvent, map, Observable, Observer, of, Subject, Subscription } from 'rxjs';
 @Component({
   selector: 'app-search-menu',
   templateUrl: './search-menu.component.html',
-  styleUrls: ['./search-menu.component.css']
+  styleUrls: ['./search-menu.component.css'],
+
 })
 export class SearchMenuComponent implements OnInit {
 
@@ -11,10 +12,31 @@ export class SearchMenuComponent implements OnInit {
   search_input_boolean: boolean = true;
   search_boolean$ = of(this.search_input_boolean)
 
+
+  media_input_boolean: boolean = false;
+  media_input$ = of(this.media_input_boolean);
+
+
+
   constructor() { }
+
+
+
+  mediaAddClass(){
+    if(window.innerWidth < 654 && !this.media_input_boolean) {
+      this.media_input$.subscribe(() => this.media_input_boolean = true)
+    } else {this.media_input_boolean = false}
+  }
+
+
+
+
 
   ngOnInit(): void {
   }
+
+
+
 
   add(){
     this.search_boolean$.subscribe(() => this.search_input_boolean = false)
@@ -23,4 +45,8 @@ export class SearchMenuComponent implements OnInit {
     this.search_boolean$.subscribe(() => this.search_input_boolean = true)
   }
  
+
+
+
+
 }
